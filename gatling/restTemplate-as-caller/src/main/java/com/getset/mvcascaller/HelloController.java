f8061c40d55a1d5d2107212433cb2ec9f0dd5433
get-set <535993116@qq.com>
2018-03-01 18:37:54 +0800

    Create RestTemplate with pooled connectionManager

--- gatling/restTemplate-as-caller/src/main/java/com/getset/mvcascaller/HelloController.java
+++ gatling/restTemplate-as-caller/src/main/java/com/getset/mvcascaller/HelloController.java
@@ -1,5 +1,8 @@
 package com.getset.mvcascaller;
 
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +16,20 @@ public class HelloController {
     private RestTemplate restTemplate;
 
     public HelloController() {
-        this.restTemplate = new RestTemplate();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultMaxPerRoute(1000);
+        connectionManager.setMaxTotal(1000);
+        this.restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().setConnectionManager(connectionManager).build()
+        ));
+
     }
 
     @GetMapping("/hello/{latency}")
-    public Mono<String> hello(@PathVariable int latency) {
+//    public String hello(@PathVariable int latency) {
 //        return restTemplate.getForObject(TARGET_HOST + "/hello/" + latency, String.class);
+//    }
+    public Mono<String> hello(@PathVariable int latency) {
         return Mono.fromCallable(() -> restTemplate.getForObject(TARGET_HOST + "/hello/" + latency, String.class))
                 .subscribeOn(Schedulers.elastic());
     }
