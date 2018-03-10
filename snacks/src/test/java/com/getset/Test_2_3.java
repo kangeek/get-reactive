ea0b9734332d8c4497df43e34f4f938962faf546
get-set <535993116@qq.com>
2018-03-10 20:40:42 +0800

    Add tests for chapter 2.3

---
+++ snacks/src/test/java/com/getset/Test_2_3.java
@@ -0,0 +1,154 @@
+package com.getset;
+
+import com.getset.chapter_2_2.MyEventListener;
+import com.getset.chapter_2_2.MyEventSource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Test_2_3 {
+
+    private final int EVENT_DURATION   = 10;    // 生成的事件间隔时间，单位毫秒
+    private final int EVENT_COUNT      = 20;    // 生成的事件个数
+    private final int PROCESS_DURATION = 30;    // 订阅者处理每个元素的时间，单位毫秒
+
+    private Flux<MyEventSource.MyEvent> fastPublisher;
+    private SlowSubscriber slowSubscriber;
+    private MyEventSource eventSource;
+    private CountDownLatch countDownLatch;
+
+    /**
+     * 准备工作。
+     */
+    @Before
+    public void setup() {
+        countDownLatch = new CountDownLatch(1);
+        slowSubscriber = new SlowSubscriber();
+        eventSource = new MyEventSource();
+    }
+
+    /**
+     * 触发订阅，使用CountDownLatch等待订阅者处理完成。
+     */
+    @After
+    public void subscribe() throws InterruptedException {
+        fastPublisher.subscribe(slowSubscriber);
+        generateEvent(EVENT_COUNT, EVENT_DURATION);
+        countDownLatch.await(1, TimeUnit.MINUTES);
+    }
+
+
+    /**
+     * 测试create方法的不同OverflowStrategy的效果。
+     */
+    @Test
+    public void testCreateBackPressureStratety() {
+        fastPublisher =
+                createFlux(FluxSink.OverflowStrategy.IGNORE)    // BUFFER/DROP/LATEST/ERROR/IGNORE
+                        .doOnRequest(n -> System.out.println("         ===  request: " + n + " ==="))
+                        .publishOn(Schedulers.newSingle("newSingle"), 2);
+    }
+
+    /**
+     * 测试不同的onBackpressureXxx方法的效果。
+     */
+    @Test
+    public void testOnBackPressureXxx() {
+        fastPublisher = createFlux(FluxSink.OverflowStrategy.BUFFER)
+//                .onBackpressureDrop()
+                .onBackpressureBuffer()
+//                .onBackpressureLatest()
+//                .onBackpressureError()
+                .doOnRequest(n -> System.out.println("         ===  request: " + n + " ==="))
+                .publishOn(Schedulers.newSingle("newSingle"), 1);
+    }
+
+    /**
+     * 其他可以实现类似效果的操作符。
+     */
+    @Test
+    public void testSimilarOperators() {
+        fastPublisher = createFlux(FluxSink.OverflowStrategy.BUFFER)
+                .doOnRequest(n -> System.out.println("         ===  request: " + n + " ==="))
+                .sample(Duration.ofMillis(30))
+        ;
+    }
+
+    /**
+     * 使用create方法生成“快的发布者”。
+     * @param strategy 回压策略
+     * @return  Flux
+     */
+    private Flux<MyEventSource.MyEvent> createFlux(FluxSink.OverflowStrategy strategy) {
+        return Flux.create(sink -> eventSource.register(new MyEventListener() {
+            @Override
+            public void onNewEvent(MyEventSource.MyEvent event) {
+                System.out.println("publish >>> " + event.getMessage());
+                sink.next(event);
+            }
+
+            @Override
+            public void onEventStopped() {
+                sink.complete();
+            }
+        }), strategy);
+    }
+
+    /**
+     * 生成MyEvent。
+     * @param count 生成MyEvent的个数。
+     * @param millis 每个MyEvent之间的时间间隔。
+     */
+    private void generateEvent(int count, int millis) {
+        for (int i = 0; i < count; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(millis);
+            } catch (InterruptedException e) {
+            }
+            eventSource.newEvent(new MyEventSource.MyEvent(new Date(), "Event-" + i));
+        }
+        eventSource.eventStopped();
+    }
+
+    /**
+     * 内部类，“慢的订阅者”。
+     */
+    class SlowSubscriber extends BaseSubscriber<MyEventSource.MyEvent> {
+
+        @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+            request(1);
+        }
+
+        @Override
+        protected void hookOnNext(MyEventSource.MyEvent event) {
+            System.out.println("                      receive <<< " + event.getMessage());
+            try {
+                TimeUnit.MILLISECONDS.sleep(PROCESS_DURATION);
+            } catch (InterruptedException e) {
+            }
+            request(1);
+        }
+
+        @Override
+        protected void hookOnError(Throwable throwable) {
+            System.err.println("                      receive <<< " + throwable);
+        }
+
+        @Override
+        protected void hookOnComplete() {
+            countDownLatch.countDown();
+        }
+    }
+
+}
